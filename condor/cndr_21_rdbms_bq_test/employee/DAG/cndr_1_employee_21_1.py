583445af48618ed8cdd9999d02794ef986691efc
PRAJKTARAJE DESHMUKH <30643766+prajktaraje@users.noreply.github.com>
2025-09-08 17:22:54 +0530

    Update DAG CNDR_1_EMPLOYEE_21_1

--- condor/cndr_21_rdbms_bq_test/employee/DAG/cndr_1_employee_21_1.py
+++ condor/cndr_21_rdbms_bq_test/employee/DAG/cndr_1_employee_21_1.py
@@ -53,7 +53,7 @@ local_tz = pendulum.timezone('Asia/Kolkata')
 
 # DAG Definition
 dag = DAG(
-    dag_id='CNDR_{{user_id}}_EMPLOYEE_{{wf_id}}_{{job_id}}',
+    dag_id=''CNDR_{{user_id}}_EMPLOYEE_{{wf_id}}_{{job_id}}'',
     default_args=default_dag_args,
     start_date=datetime(
         2025, 7, 20, 12, 40, tzinfo=local_tz
@@ -64,16 +64,16 @@ dag = DAG(
 
 # Dataflow Flex Template Operator
 DIF_INGESTION = StartFlexTemplateCustomOperator(
-    task_id = 'CNDR_{{user_id}}_EMPLOYEE_{{wf_id}}_{{job_id}}_BASH_TASK',
+    task_id = ''CNDR_{{user_id}}_EMPLOYEE_{{wf_id}}_{{job_id}}_BASH_TASK'',
     location = 'us-central1',
     project_id = 'dm-condor-dev',
-    job_name = "cndr-{{user_id}}-employee-{{wf_id}}-{{job_id}}-1240-m",
+    job_name = "'cndr-{{user_id}}-employee-{{wf_id}}-{{job_id}}-1240-m'",
     container_spec_gcs_path = 'gs://condor_dev_gcs/onix_condor_core_flex_template_v1.json',
     parameters = {
              "project": "dm-condor-dev",
              "task_prop_table": "dm-condor-dev.condor_dev.task_prop_table",
              "bq_project": "dm-condor-dev",
-             "task_id": "CNDR_{{user_id}}_EMPLOYEE_{{wf_id}}_{{job_id}}",
+             "task_id": "'CNDR_{{user_id}}_EMPLOYEE_{{wf_id}}_{{job_id}}'",
     },
     environment = {
                 "tempLocation": "gs://condor_dev_gcs/temp/",
